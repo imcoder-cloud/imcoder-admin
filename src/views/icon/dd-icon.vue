2ad64aa3b6b06569862bf1693ecbe3d885a6e990
dongdongchen <dd18355193335@163.com>
2019-10-28 18:00:45 +0800

    增加字体图标

--- src/views/icon/dd-icon.vue
+++ src/views/icon/dd-icon.vue
@@ -9,6 +9,194 @@
         <i class="dd cancel-full-screen"></i>
         cancel-full-screen
       </div>
+      <div class="icon-item">
+        <i class="dd pie"></i>
+        pie
+      </div>
+      <div class="icon-item">
+        <i class="dd cash"></i>
+        cash
+      </div>
+      <div class="icon-item">
+        <i class="dd enter"></i>
+        enter
+      </div>
+      <div class="icon-item">
+        <i class="dd promotion"></i>
+        promotion
+      </div>
+      <div class="icon-item">
+        <i class="dd stock"></i>
+        stock
+      </div>
+      <div class="icon-item">
+        <i class="dd qr-code"></i>
+        qr-code
+      </div>
+      <div class="icon-item">
+        <i class="dd print-order"></i>
+        print-order
+      </div>
+      <div class="icon-item">
+        <i class="dd payback"></i>
+        payback
+      </div>
+      <div class="icon-item">
+        <i class="dd work-card"></i>
+        work-card
+      </div>
+      <div class="icon-item">
+        <i class="dd order"></i>
+        order
+      </div>
+      <div class="icon-item">
+        <i class="dd user-data"></i>
+        user-data
+      </div>
+      <div class="icon-item">
+        <i class="dd customer"></i>
+        customer
+      </div>
+      <div class="icon-item">
+        <i class="dd reimburse"></i>
+        reimburse
+      </div>
+      <div class="icon-item">
+        <i class="dd data-statistics"></i>
+        data-statistics
+      </div>
+      <div class="icon-item">
+        <i class="dd operation"></i>
+        operation
+      </div>
+      <div class="icon-item">
+        <i class="dd conditioner"></i>
+        conditioner
+      </div>
+      <div class="icon-item">
+        <i class="dd users"></i>
+        users
+      </div>
+      <div class="icon-item">
+        <i class="dd receive-payment"></i>
+        receive-payment
+      </div>
+      <div class="icon-item">
+        <i class="dd computer-settings"></i>
+        computer-settings
+      </div>
+      <div class="icon-item">
+        <i class="dd oa"></i>
+        oa
+      </div>
+      <div class="icon-item">
+        <i class="dd budget"></i>
+        budget
+      </div>
+      <div class="icon-item">
+        <i class="dd warehouse"></i>
+        warehouse
+      </div>
+      <div class="icon-item">
+        <i class="dd transport"></i>
+        transport
+      </div>
+      <div class="icon-item">
+        <i class="dd transport-data"></i>
+        transport-data
+      </div>
+      <div class="icon-item">
+        <i class="dd delivery"></i>
+        delivery
+      </div>
+      <div class="icon-item">
+        <i class="dd activity"></i>
+        activity
+      </div>
+      <div class="icon-item">
+        <i class="dd fingerprint"></i>
+        fingerprint
+      </div>
+      <div class="icon-item">
+        <i class="dd pending-receipt"></i>
+        pending-receipt
+      </div>
+      <div class="icon-item">
+        <i class="dd pending-order"></i>
+        pending-order
+      </div>
+      <div class="icon-item">
+        <i class="dd to-be-delivered"></i>
+        to-be-delivered
+      </div>
+      <div class="icon-item">
+        <i class="dd to-do-item"></i>
+        to-do-item
+      </div>
+      <div class="icon-item">
+        <i class="dd payment"></i>
+        payment
+      </div>
+      <div class="icon-item">
+        <i class="dd food"></i>
+        food
+      </div>
+      <div class="icon-item">
+        <i class="dd bill"></i>
+        bill
+      </div>
+      <div class="icon-item">
+        <i class="dd shop"></i>
+        shop
+      </div>
+      <div class="icon-item">
+        <i class="dd order-edit"></i>
+        order-edit
+      </div>
+      <div class="icon-item">
+        <i class="dd received"></i>
+        received
+      </div>
+      <div class="icon-item">
+        <i class="dd microphone"></i>
+        microphone
+      </div>
+      <div class="icon-item">
+        <i class="dd banquet"></i>
+        banquet
+      </div>
+      <div class="icon-item">
+        <i class="dd meeting"></i>
+        meeting
+      </div>
+      <div class="icon-item">
+        <i class="dd user-center"></i>
+        user-center
+      </div>
+      <div class="icon-item">
+        <i class="dd settings"></i>
+        settings
+      </div>
+      <div class="icon-item">
+        <i class="dd delivery"></i>
+        delivery
+      </div>
+      <div class="icon-item">
+        <i class="dd download"></i>
+        download
+      </div>
+      <div class="icon-item">
+        <i class="dd cart"></i>
+        cart
+      </div>
+      <div class="icon-item">
+        <i class="dd pencil"></i>
+        pencil
+      </div>
+      <div class="icon-item">
+        <i class="dd delete"></i>
+        delete
+      </div>
     </div>
   </div>
 </template>
